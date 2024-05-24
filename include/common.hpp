f8670f0b8d922f3ff586c5ac31000800feaa9518
Alexei Zakharov <alexeiz@gmail.com>
2024-05-23 23:01:55 -0400

    add flux to dependencies

--- include/common.hpp
+++ include/common.hpp
@@ -25,5 +25,7 @@
 #include <fmt/core.h>
 #include <fmt/ostream.h>
 
+#include <flux.hpp>
+
 template<class... Ts> struct overloaded : Ts... { using Ts::operator()...; };
 template<class... Ts> overloaded(Ts...) -> overloaded<Ts...>;
